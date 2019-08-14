8458785a3a9f3351cc18c05285388c7e7937246e
agata <agata.wiewiora1@gmail.com>
2019-08-14 20:24:24 +0200

    updated with sample blog app (hello app)

--- hello/admin.py
+++ hello/admin.py
@@ -1,7 +1,19 @@
 from django.contrib import admin
 from .models import Person
 from .models import Post
+from .models import Comment
 
 # Register your models here.
 admin.site.register(Person)
-admin.site.register(Post)
+#admin.site.register(Post)
+admin.site.register(Comment)
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_on')
+    list_filter = ('status',)
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, PostAdmin)
